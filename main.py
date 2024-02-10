6075bd10acd9d4eacf760d53ac1ebc71df656649
Veysel Üstüntaş <veyselustuntas1@gmail.com>
2024-02-10 23:33:06 +0300

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+def kullanici_ile_etkilesim():
+    kullanici_adi = input("Merhaba! Lütfen adınızı girin: ")
+
+    selamlama_mesaji = f"Merhaba, {kullanici_adi}! Hoş geldin."
+
+    print(selamlama_mesaji)
+
+kullanici_ile_etkilesim()
